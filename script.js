7d20b9f3d39d0a0c8125f8ffd803418370b29e6e
Natalie <natalie.dflcn@gmail.com>
2024-01-07 19:52:10 -0500

    Added event listener for rolling the dice

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+//Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+const btnNew = document.querySelector('btn--new');
+const btnRoll = document.querySelector('btn--roll');
+const btnHold = document.querySelector('btn--hold');
+const currScore0 = document.querySelector('current--0');
+const currScore1 = document.querySelector('current--1');
+
+//Event Handlers
+
+//Starting conditions
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll = addEventListener('click', function () {
+  const num = Math.floor(Math.random() * 6) + 1;
+  console.log(num);
+
+  dice.classList.remove('hidden');
+  dice.src = `dice-${num}.png`;
+
+  if (num != 1) {
+    currentScore += num;
+  } else {
+    currentScore = 0;
+  }
+});
